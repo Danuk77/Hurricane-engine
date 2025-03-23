13acbd0e60ceb4922baf27731b5238f4041facf1
Danuk <danuk77@live.co.uk>
2025-03-23 14:05:08 +0000

    Cleaned up the test code

--- tests/physics/collistion_detection/test_box_box_detector.cpp
+++ tests/physics/collistion_detection/test_box_box_detector.cpp
@@ -1,5 +1,6 @@
-// clang-format off
 #include <gtest/gtest.h>
+// clang-format off
+#include <algorithm>
 // clang-format on
 
 #include <memory>
@@ -9,6 +10,13 @@
 #include "physics/colliders/box_collider.hpp"
 #include "physics/colliders/factories/box_collider_factory.hpp"
 #include "physics/collision_detection/detectors/box_box_detector.hpp"
+
+std::shared_ptr<BoxCollider> make_test_collider(glm::vec2 position, float width,
+                                                float height) {
+  return std::shared_ptr<BoxCollider>(create_box_collider(
+      width, height,
+      std::make_shared<Transform>(position, glm::vec2(width, height))));
+}
 
 // ###############################################
 // ###############################################
@@ -35,45 +43,16 @@ INSTANTIATE_TEST_CASE_P(
     CollisionTests, CollisionTestParametrizedFixture,
     ::testing::Values(
         // TEST 1
-        std::make_tuple(std::shared_ptr<BoxCollider>(create_box_collider(
-                            100, 100,
-                            std::make_shared<Transform>(glm::vec2(10, 10),
-                                                        glm::vec2(100, 100)))),
-                        std::shared_ptr<BoxCollider>(create_box_collider(
-                            100, 100,
-                            std::make_shared<Transform>(glm::vec2(50, 100),
-                                                        glm::vec2(100, 100)))),
-                        true),
+        std::make_tuple(make_test_collider(glm::vec2(10, 10), 100, 100),
+                        make_test_collider(glm::vec2(50, 100), 100, 100), true),
         // TEST 2
-        std::make_tuple(std::shared_ptr<BoxCollider>(create_box_collider(
-                            30, 30,
-                            std::make_shared<Transform>(glm::vec2(10, 0),
-                                                        glm::vec2(30, 30)))),
-                        std::shared_ptr<BoxCollider>(create_box_collider(
-                            30, 30,
-                            std::make_shared<Transform>(glm::vec2(80, 20),
-                                                        glm::vec2(30, 30)))),
-                        false),
+        std::make_tuple(make_test_collider(glm::vec2(10, 0), 30, 30),
+                        make_test_collider(glm::vec2(80, 20), 30, 30), false),
         // TEST 3
-        std::make_tuple(std::shared_ptr<BoxCollider>(create_box_collider(
-                            10, 10,
-                            std::make_shared<Transform>(glm::vec2(0, 0),
-                                                        glm::vec2(10, 10)))),
-                        std::shared_ptr<BoxCollider>(create_box_collider(
-                            50, 50,
-                            std::make_shared<Transform>(glm::vec2(-10, -10),
-                                                        glm::vec2(50, 50)))),
-                        true),
-        // TEST 4
-        std::make_tuple(std::shared_ptr<BoxCollider>(create_box_collider(
-                            10, 10,
-                            std::make_shared<Transform>(glm::vec2(0, 0),
-                                                        glm::vec2(10, 10)))),
-                        std::shared_ptr<BoxCollider>(create_box_collider(
-                            10, 20,
-                            std::make_shared<Transform>(glm::vec2(0, 50),
-                                                        glm::vec2(0, 20)))),
-                        false)));
+        std::make_tuple(make_test_collider(glm::vec2(0, 0), 10, 10),
+                        make_test_collider(glm::vec2(-10, -10), 50, 50), true),
+        std::make_tuple(make_test_collider(glm::vec2(0, 0), 10, 10),
+                        make_test_collider(glm::vec2(0, 50), 20, 20), false)));
 
 // #########################################################
 // #########################################################
@@ -81,6 +60,40 @@ INSTANTIATE_TEST_CASE_P(
 // #########################################################
 // #########################################################
 
-class CollisionNormalCalculation
-    : public ::testing::TestWithParam<std::tuple<
-          std::shared_ptr<BoxCollider>, std::shared_ptr<BoxCollider>, bool>> {};
+// class CollisionNormalCalculation
+//     : public ::testing::TestWithParam<
+//           std::tuple<std::shared_ptr<BoxCollider>,
+//           std::shared_ptr<BoxCollider>,
+//                      glm::vec2>> {};
+//
+// TEST_P(CollisionNormalCalculation, TEST_CALCULATING_COLLISION_NORMAL) {
+//   std::shared_ptr<BoxCollider> collider_one = std::get<0>(GetParam());
+//   std::shared_ptr<BoxCollider> collider_two = std::get<1>(GetParam());
+//
+//   glm::vec2 expected_collision_normal = std::get<2>(GetParam());
+//   glm::vec2 collision_normal =
+//   collision_detection::calculate_collision_normal(
+//       collider_one.get(), collider_two.get());
+//
+//   ASSERT_EQ(collision_normal, expected_collision_normal);
+// }
+//
+//
+// INSTANTIATE_TEST_CASE_P(
+//     CollisionTests, CollisionNormalCalculation,
+//     ::testing::Values(
+//         // TEST 1
+//         std::make_tuple(std::shared_ptr<BoxCollider>(make_test_collider(glm::vec2(10,
+//         10), 100, 100)),
+//                         std::shared_ptr<BoxCollider>(make_test_collider(glm::vec2(50,
+//                         100), 100, 100)), true),
+//         // TEST 2
+//         std::make_tuple(std::shared_ptr<BoxCollider>(create_box_collider(
+//                             30, 30,
+//                             std::make_shared<Transform>(glm::vec2(10, 0),
+//                                                         glm::vec2(30, 30)))),
+//                         std::shared_ptr<BoxCollider>(create_box_collider(
+//                             30, 30,
+//                             std::make_shared<Transform>(glm::vec2(80, 20),
+//                                                         glm::vec2(30, 30)))),
+//                         false)));
